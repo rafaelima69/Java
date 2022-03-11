4c421f9d30729c5244dc1a5ec88daaec12532918
rafaelima69 <97762806+rafaelima69@users.noreply.github.com>
2022-03-11 09:05:45 -0300

    Add files via upload

---
+++ Atv01.java
@@ -0,0 +1,41 @@
+package br.com.generation.AtvEncapsulamento;
+
+public class Atv01 {
+	
+private String nome;
+private int idade;
+private String endereco;
+private String cpf;
+
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public int getIdade() {
+	return idade;
+}
+public void setIdade(int idade) {
+	this.idade = idade;
+}
+public String getEndereco() {
+	return endereco;
+}
+public void setEndereco(String endereco) {
+	this.endereco = endereco;
+}
+public String getCpf() {
+	return cpf;
+}
+public void setCpf(String cpf) {
+	this.cpf = cpf;
+}
+
+
+
+
+
+
+
+}
